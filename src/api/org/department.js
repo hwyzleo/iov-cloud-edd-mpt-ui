e716dd2e6241b9f6563d91ce380920b0048260b0
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-05-15 10:57:43 +0800

    fix: add Chinese comments and trailing newline to department API

--- src/api/org/department.js
+++ src/api/org/department.js
@@ -1,5 +1,6 @@
 import request from '@/utils/request'
 
+// 查询部门列表
 export function listDept(query) {
   return request({
     url: '/mpt/department/list',
@@ -8,6 +9,7 @@ export function listDept(query) {
   })
 }
 
+// 查询部门树
 export function getDeptTree(query) {
   return request({
     url: '/mpt/department/tree',
@@ -16,6 +18,7 @@ export function getDeptTree(query) {
   })
 }
 
+// 查询部门列表（排除节点）
 export function listDeptExcludeChild(deptId) {
   return request({
     url: '/mpt/department/list/exclude/' + deptId,
@@ -23,6 +26,7 @@ export function listDeptExcludeChild(deptId) {
   })
 }
 
+// 查询部门详细
 export function getDept(deptId) {
   return request({
     url: '/mpt/department/' + deptId,
@@ -30,6 +34,7 @@ export function getDept(deptId) {
   })
 }
 
+// 新增部门
 export function addDept(data) {
   return request({
     url: '/mpt/department',
@@ -38,6 +43,7 @@ export function addDept(data) {
   })
 }
 
+// 修改部门
 export function updateDept(data) {
   return request({
     url: '/mpt/department',
@@ -46,6 +52,7 @@ export function updateDept(data) {
   })
 }
 
+// 删除部门
 export function delDept(deptId) {
   return request({
     url: '/mpt/department/' + deptId,
@@ -53,6 +60,7 @@ export function delDept(deptId) {
   })
 }
 
+// 查询部门下拉树结构
 export function deptTreeSelect(query) {
   return request({
     url: '/mpt/department/deptTree',
