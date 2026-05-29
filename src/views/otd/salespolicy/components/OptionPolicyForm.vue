881b3186005784a6499c566d5be06087ea7fb276
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-05-30 00:06:34 +0800

    feat: create OptionPolicyForm component

---
+++ src/views/otd/salespolicy/components/OptionPolicyForm.vue
@@ -0,0 +1,169 @@
+<template>
+  <el-dialog :title="title" :visible.sync="open" width="600px" append-to-body>
+    <el-form ref="form" :model="form" :rules="rules" label-width="120px">
+      <el-form-item label="选项代码" prop="optionCode">
+        <el-input v-model="form.optionCode" :readonly="form.id !== undefined" placeholder="请输入选项代码"/>
+      </el-form-item>
+      <el-form-item label="销售状态" prop="saleStatus">
+        <el-select v-model="form.saleStatus" placeholder="请选择销售状态">
+          <el-option label="上架" value="active"/>
+          <el-option label="下架" value="off_shelf"/>
+          <el-option label="即将上市" value="coming_soon"/>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="选项价格" prop="optionPrice">
+        <el-input-number v-model="form.optionPrice" :precision="2" :min="0" controls-position="right"/>
+      </el-form-item>
+      <el-form-item label="可售区域">
+        <el-input v-model="form.availableRegions" placeholder="请输入可售区域，多个用逗号分隔"/>
+      </el-form-item>
+      <el-form-item label="销售渠道">
+        <el-input v-model="form.channels" placeholder="请输入销售渠道，多个用逗号分隔"/>
+      </el-form-item>
+      <el-form-item label="捆绑销售">
+        <el-input v-model="form.bundleWith" placeholder="请输入捆绑销售选项代码，多个用逗号分隔"/>
+      </el-form-item>
+      <el-form-item label="互斥选项">
+        <el-input v-model="form.mutexWith" placeholder="请输入互斥选项代码，多个用逗号分隔"/>
+      </el-form-item>
+      <el-form-item label="营销标题">
+        <el-input v-model="form.marketingTitle" placeholder="请输入营销标题"/>
+      </el-form-item>
+      <el-form-item label="营销图片">
+        <el-input v-model="form.marketingImage" placeholder="请输入营销图片URL"/>
+      </el-form-item>
+      <el-form-item label="排序权重">
+        <el-input-number v-model="form.sortWeight" controls-position="right" :min="0"/>
+      </el-form-item>
+      <el-form-item label="生效时间">
+        <el-date-picker v-model="form.effectiveFrom" type="datetime" placeholder="请选择生效时间"/>
+      </el-form-item>
+      <el-form-item label="失效时间">
+        <el-date-picker v-model="form.effectiveTo" type="datetime" placeholder="请选择失效时间"/>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button type="primary" @click="submitForm">确 定</el-button>
+      <el-button @click="cancel">取 消</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+import { createOptionPolicy, updateOptionPolicy } from '@/api/otd/salespolicy'
+
+export default {
+  name: 'OptionPolicyForm',
+  props: {
+    open: {
+      type: Boolean,
+      default: false
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    formData: {
+      type: Object,
+      default: () => ({})
+    },
+    saleModelCode: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      form: {
+        id: undefined,
+        optionCode: undefined,
+        saleStatus: 'active',
+        optionPrice: 0,
+        availableRegions: undefined,
+        channels: undefined,
+        bundleWith: undefined,
+        mutexWith: undefined,
+        marketingTitle: undefined,
+        marketingImage: undefined,
+        sortWeight: 0,
+        effectiveFrom: undefined,
+        effectiveTo: undefined
+      },
+      rules: {
+        optionCode: [
+          { required: true, message: '选项代码不能为空', trigger: 'blur' }
+        ],
+        saleStatus: [
+          { required: true, message: '销售状态不能为空', trigger: 'change' }
+        ],
+        optionPrice: [
+          { required: true, message: '选项价格不能为空', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  watch: {
+    formData: {
+      handler(val) {
+        if (val && Object.keys(val).length > 0) {
+          this.form = { ...val }
+        } else {
+          this.resetForm()
+        }
+      },
+      deep: true,
+      immediate: true
+    }
+  },
+  methods: {
+    resetForm() {
+      this.form = {
+        id: undefined,
+        optionCode: undefined,
+        saleStatus: 'active',
+        optionPrice: 0,
+        availableRegions: undefined,
+        channels: undefined,
+        bundleWith: undefined,
+        mutexWith: undefined,
+        marketingTitle: undefined,
+        marketingImage: undefined,
+        sortWeight: 0,
+        effectiveFrom: undefined,
+        effectiveTo: undefined
+      }
+    },
+    submitForm() {
+      this.$refs['form'].validate(valid => {
+        if (valid) {
+          // 校验bundleWith和mutexWith是否矛盾
+          if (this.form.bundleWith && this.form.mutexWith) {
+            const bundleWithArr = this.form.bundleWith.split(',')
+            const mutexWithArr = this.form.mutexWith.split(',')
+            const conflict = bundleWithArr.some(item => mutexWithArr.includes(item))
+            if (conflict) {
+              this.$modal.msgError('bundleWith和mutexWith存在矛盾配置')
+              return
+            }
+          }
+
+          if (this.form.id !== undefined) {
+            updateOptionPolicy(this.saleModelCode, this.form.id, this.form).then(response => {
+              this.$modal.msgSuccess('修改成功')
+              this.$emit('success')
+            })
+          } else {
+            createOptionPolicy(this.saleModelCode, this.form).then(response => {
+              this.$modal.msgSuccess('新增成功')
+              this.$emit('success')
+            })
+          }
+        }
+      })
+    },
+    cancel() {
+      this.$emit('update:open', false)
+    }
+  }
+}
+</script>
